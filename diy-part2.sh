1cf86fbddb0e5259ab76605ec2ddef77356267bf
fzh5201413 <46644112+fzh5201413@users.noreply.github.com>
2023-02-04 19:42:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.3.1/g' package/base-files/files/bin/config_generate
+
+# Modify default Theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
