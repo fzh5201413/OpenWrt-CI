f09aa6042f44553b256273afd18168e45329a115
fzh <2268599429@qq.com>
2024-10-10 11:31:31 +0800

    fix: remove bandwidthd

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+###
+ # @Author: fzh 2268599429@qq.com
+ # @Date: 2024-10-10 11:16:51
+ # @LastEditors: fzh 2268599429@qq.com
+ # @LastEditTime: 2024-10-10 11:30:53
+ # @FilePath: \OpenWrt-CI\diy-part1.sh
+ # @Description: 
+ # 
+ # Copyright (c) 2024 by fzh, All Rights Reserved. 
+### 
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -19,4 +29,3 @@ sed -i 's/^#\(.*vernesong\)/\1/' feeds.conf.default
 echo "src-git my_packages https://github.com/openwrt/packages.git" >> "feeds.conf.default"
 echo "src-git vernesong https://github.com/vernesong/OpenClash.git" >> "feeds.conf.default"
 echo "src-git zerotier https://github.com/mwarning/zerotier-openwrt.git" >> "feeds.conf.default"
-echo "src-git bandwidthd https://github.com/AlexZhuo/luci-app-bandwidthd.git >> "feeds.conf.default"
